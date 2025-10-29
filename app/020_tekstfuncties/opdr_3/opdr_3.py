1b2c8f89934bc7de6d2515cdaeaa086105798606
vkuchuk1 <97123356@st.deltion.nl>
2025-10-29 14:08:10 +0100

    opdr3 klaar

--- app/020_tekstfuncties/opdr_3/opdr_3.py
+++ app/020_tekstfuncties/opdr_3/opdr_3.py
@@ -3,3 +3,16 @@
 # Groep:
 
 # Hier komt je code...
+
+a = ("     *     ") 
+b = ("    ***    ")
+c = ("   *****   ")
+d = ("  *******  ")
+e = (" ********* ")
+f = ("    ***    ")   
+g = ("    ***    ")
+h = ("    ***    ")
+
+my_kerst = (a, b, c, d, e, f, g, h)
+for line in my_kerst:
+    print(line * 5)
