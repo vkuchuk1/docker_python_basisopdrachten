18d4e958bfe5b633004d314d6482789213d18f30
vkuchuk1 <97123356@st.deltion.nl>
2025-11-06 13:57:57 +0100

    opdr1

--- app/030_berekeningen/opdr_1/opdr_1.py
+++ app/030_berekeningen/opdr_1/opdr_1.py
@@ -3,3 +3,22 @@
 # Groep:
 
 # Hier komt je code...
+
+vriends = ["joe", "bob", "fred"]
+leeftijd_joe = 23
+leeftijd_bob = 15
+leeftijd_fred = 22
+for vriend in vriends:
+    vriend1 = "joe"
+    vriend2 = "bob"
+    vriend3 = "fred"
+    if vriend == "joe":
+        print(vriend, "=", leeftijd_joe)
+    elif vriend == "bob":
+        print(vriend, "=", leeftijd_bob)
+    elif vriend == "fred":
+        print(vriend, "=", leeftijd_fred)
+print(f"Dit weten we over", vriend1 , vriend2, "en",  vriend3)
+total = leeftijd_joe + leeftijd_bob + leeftijd_fred
+print(f"De totale waarde van alle leeftijden bij elkaar opgeteld = ", total)
+print(f"De gemiddelde leeftijd van deze boys is: {int(total / 3)}")
