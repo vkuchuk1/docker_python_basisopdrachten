80505441e5f9e86b997bf7ef427d857190005d80
vkuchuk1 <97123356@st.deltion.nl>
2025-11-06 14:13:27 +0100

    opdr2

--- app/030_berekeningen/opdr_2/opdr_2.py
+++ app/030_berekeningen/opdr_2/opdr_2.py
@@ -3,8 +3,15 @@
 # Groep:
 
 # Hier komt je code...
-
-c = ...
-f = ...
-
-print()
+try:
+    c = int(input("Voer een Celsius hier: "))
+    f = int(input("Voer een Fahrenheit hier: "))
+    
+    print("c =", c)
+    print("f =", f)
+except ValueError: 
+    print("Verkeerde invoer")
+celtofar = (c * 9/5) + 32
+fartocel = (f - 32) * 5/9
+print(f"{c} graden Celsius is gelijk aan {celtofar} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {fartocel} graden Celsius")
