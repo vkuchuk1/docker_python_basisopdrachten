82bc86b0bd3f0fe8fae479874f4a0f6fca073f1a
vkuchuk1 <97123356@st.deltion.nl>
2026-08-27 15:09:43 +0200

    030

--- app/030_berekeningen/opdr_1/opdr_1.py
+++ app/030_berekeningen/opdr_1/opdr_1.py
@@ -3,7 +3,7 @@
 # Groep:
 
 # Hier komt je code...
-
+"""
 vriends = {"vriend1": "joe", "vriend2": "bob", "vriend3": "fred"}
 leeftijd_joe = 23
 leeftijd_bob = 15
@@ -15,3 +15,13 @@ print(f"Dit weten we over {vriends['vriend1']}, {vriends['vriend2']} en {vriends
 total = leeftijd_joe + leeftijd_bob + leeftijd_fred
 print(f"De totale waarde van alle leeftijden bij elkaar opgeteld = ", total)
 print(f"De gemiddelde leeftijd van deze boys is: {int(total / 3)}")
+"""
+vriend1 = "joe"
+vriend2 = "bob"
+vriend3 = "fred"
+leeftijd_1 = 23
+leeftijd_2 = 15
+leeftijd_3 = 22
+print(f"Dit weten we over {vriend1}, {vriend2} en {vriend3}:")
+print("De totale waarde van alle leeftijden bij elkaar opgeteld =", leeftijd_1 + leeftijd_2 + leeftijd_3)
+print("De gemiddelde leeftijd van deze boys is:", int((leeftijd_1 + leeftijd_2 + leeftijd_3) / 3))
--- app/030_berekeningen/opdr_2/opdr_2.py
+++ app/030_berekeningen/opdr_2/opdr_2.py
@@ -4,14 +4,14 @@
 
 # Hier komt je code...
 try:
-    c = int(input("Voer een Celsius hier: "))
-    f = int(input("Voer een Fahrenheit hier: "))
+    c = float(input("Voer een Celsius hier: "))
+    f = float(input("Voer een Fahrenheit hier: "))
     
-    print("c =", c)
-    print("f =", f)
-except ValueError: 
-    print("Verkeerde invoer")
-celtofar = (c * 9/5) + 32
-fartocel = (f - 32) * 5/9
-print(f"{c} graden Celsius is gelijk aan {celtofar} graden Fahrenheit")
-print(f"{f} graden Fahrenheit is gelijk aan {fartocel} graden Celsius")
+    celtofar = (c * 9/5) + 32
+    fartocel = (f - 32) * 5/9
+
+    print(f"{c} graden Celsius is gelijk aan {celtofar} graden Fahrenheit")
+    print(f"{f} graden Fahrenheit is gelijk aan {fartocel} graden Celsius")
+
+except ValueError:
+    print("Geen cijfer ingevoerd")
