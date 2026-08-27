149aa1472ca2ddc46834caa514c49ee350b24df9
vkuchuk1 <97123356@st.deltion.nl>
2026-08-27 15:14:50 +0200

    030

--- app/030_berekeningen/opdr_3/opdr_3.py
+++ app/030_berekeningen/opdr_3/opdr_3.py
@@ -4,14 +4,6 @@
 
 # Hier komt je code...
 import math
-x = 1
-y = (4*(x**3)) - (2*(x**2)) + 1
-print("De uitkomst is:", y)
-
-x = 2
-y = (4*(x**3)) - (2*(x**2)) + 1
-print("De uitkomst is:", y)
-
-x = 0
+x = int(input("x = "))
 y = (4*(x**3)) - (2*(x**2)) + 1
 print("De uitkomst is:", y)
