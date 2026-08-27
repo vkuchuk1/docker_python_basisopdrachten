e23cfa69f71ccd2898a7f9c963773854f8d22569
vkuchuk1 <97123356@st.deltion.nl>
2026-08-27 15:26:36 +0200

    040

--- app/040_lists/opdr_1/opdr_1.py
+++ app/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,13 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+dict_1 = {"id": 0, "naam": "Alice", "achternaam": "Smith"}
+dict_2 = {"id": 1, "naam": "Bob", "achternaam": "Johnson",}
+dict_3 = {"id": 2, "naam": "Charlie", "achternaam": "Williams"}
+dict_4 = {"id": 3, "naam": "Diana", "achternaam": "Brown",}
+
+mylist = [dict_1, dict_2, dict_3, dict_4]
+
+id = int(input("Vul ID in: "))
+
+print(mylist[id])
