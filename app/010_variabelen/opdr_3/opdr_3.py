fd345cc635c9f4de6eecf3f3f29fea2c09664d4c
vkuchuk1 <97123356@st.deltion.nl>
2026-08-27 14:43:31 +0200

    opdr3

--- app/010_variabelen/opdr_3/opdr_3.py
+++ app/010_variabelen/opdr_3/opdr_3.py
@@ -5,13 +5,7 @@
 # Hier komt je code...
 
 my_dictionary = {"naam": "willem", "achternaam": "van der broek", "leeftijd": 23, "favoriete kleur": "blauw"}
-print(f"naam: {my_dictionary["naam"]}")
-print(f"achternaam: {my_dictionary["achternaam"]}")
-print(f"leeftijd: {my_dictionary["leeftijd"]}")
-print(f"favoriete kleur: {my_dictionary["favoriete kleur"]}")
-
-
-#my_dictionary = {"naam = willem", "achternaam = van der broek", "leeftijd = 23", "favoriete kleur = blauw"}
-#for dictionary in my_dictionary:
-#    print(dictionary)
-#exit()
+print(f"naam = {my_dictionary['naam']}")
+print(f"achternaam = {my_dictionary['achternaam']}")
+print(f"leeftijd = {my_dictionary['leeftijd']}")
+print(f"favoriete kleur = {my_dictionary['favoriete kleur']}")
