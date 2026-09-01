ba5ab43c756615658fa555cd7b9a5b1c1032b187
vkuchuk1 <97123356@st.deltion.nl>
2026-09-01 16:25:46 +0200

    040

--- app/040_lists/opdr_2/opdr_2.py
+++ app/040_lists/opdr_2/opdr_2.py
@@ -13,3 +13,8 @@ rivieren = list(rivier_info.keys())
 # rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
 # Hier jouw code.....
+print("De rivier " + rivieren[0].title() + " loopt door onder " + rivier_info[rivieren[0]][1].title())
+
+print("De rivier " + rivieren[1].title() + " loopt door onder " + rivier_info[rivieren[1]][0].title())
+
+print("De rivier " + rivieren[2].title() + " loopt door onder " + rivier_info[rivieren[2]][2].title())
