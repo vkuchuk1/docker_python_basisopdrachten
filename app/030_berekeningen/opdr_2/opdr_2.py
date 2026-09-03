d1afcc5d8849801065995698ea25008c20870bf2
vkuchuk1 <97123356@st.deltion.nl>
2026-09-03 14:45:27 +0200

    ,,

--- app/030_berekeningen/opdr_2/opdr_2.py
+++ app/030_berekeningen/opdr_2/opdr_2.py
@@ -3,15 +3,11 @@
 # Groep: 4ITX7
 
 # Hier komt je code...
-try:
-    c = float(input("Voer een Celsius hier: "))
-    f = float(input("Voer een Fahrenheit hier: "))
+c = float(input("Voer een Celsius hier: "))
+f = float(input("Voer een Fahrenheit hier: "))
     
-    celtofar = (c * 9/5) + 32
-    fartocel = (f - 32) * 5/9
+celtofar = (c * 9/5) + 32
+fartocel = (f - 32) * 5/9
 
-    print(f"{c} graden Celsius is gelijk aan {celtofar} graden Fahrenheit")
-    print(f"{f} graden Fahrenheit is gelijk aan {fartocel} graden Celsius")
-
-except ValueError:
-    print("Geen cijfer ingevoerd")
+print(f"{c} graden Celsius is gelijk aan {celtofar} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {fartocel} graden Celsius")
