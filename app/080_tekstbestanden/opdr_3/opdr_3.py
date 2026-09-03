bf2e1e0fd16357bb9b98bb2cf9e4a3016604b66b
vkuchuk1 <97123356@st.deltion.nl>
2026-09-03 13:44:20 +0200

    .

--- app/080_tekstbestanden/opdr_3/opdr_3.py
+++ app/080_tekstbestanden/opdr_3/opdr_3.py
@@ -4,3 +4,10 @@
 
 
 
+text = input("Voer een tekst in: ")
+alfabet = "abcdefghijklmnopqrstuvwxyz"
+out_alfabet = alfabet[5:] + alfabet[:5]
+
+trans_table = str.maketrans(alfabet, out_alfabet)
+result = text.translate(trans_table)
+print(result)
